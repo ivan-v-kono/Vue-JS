29eb2f5b4cabc167997f9a058495441937dfb28a
ivan-v-kono <58733225+ivan-v-kono@users.noreply.github.com>
2021-12-28 12:28:10 +0300

    Add files via upload

---
+++ Movie-Searcher/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <Home/>
+  </v-app>
+</template>
+
+<script>
+ import Home from "@/components/Home";
+
+  export default {
+    components: {
+      Home,
+    },
+  }
+</script>
+
